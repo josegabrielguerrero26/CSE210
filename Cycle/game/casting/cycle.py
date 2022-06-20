72fd706fab61558cbcf0ff1225bdb878e8045aca
Gabe G <97242028+josegabrielguerrero26@users.noreply.github.com>
2022-06-20 10:59:47 -0500

    cycle test1

--- Cycle/game/casting/cycle.py
+++ Cycle/game/casting/cycle.py
@@ -4,16 +4,11 @@ from game.shared.point import Point
 
 
 class Cycle(Actor):
-    """
-    A long limbless reptile.
-    
-    The responsibility of Snake is to move itself.
+    """Replacying snake with cycle"""
 
-    Attributes:
-        _points (int): The number of points the food is worth.
-    """
-    def __init__(self):
+    def __init__(self,color): # add colors
         super().__init__()
+        self._cycle_color = color # colors in cycles
         self._segments = []
         self._prepare_body()
 
@@ -52,18 +47,26 @@ class Cycle(Actor):
         self._segments[0].set_velocity(velocity)
     
     def _prepare_body(self):
-        x = int(constants.MAX_X / 2)
-        y = int(constants.MAX_Y / 2)
+        x = 0.0
+        y = 0.0
+        
+        if(self._cycle_color == constants.RED):
+            #Start position 
+            x= int(constants.MAX_X /4)
+            y= int(constants.MAX_Y/4)
+        else:
+            #Gree Cycle
+            x = int(constants.MAX_X / 2)
+            y = int(constants.MAX_Y / 2)
 
         for i in range(constants.SNAKE_LENGTH):
             position = Point(x - i * constants.CELL_SIZE, y)
             velocity = Point(1 * constants.CELL_SIZE, 0)
             text = "8" if i == 0 else "#"
-            color = constants.YELLOW if i == 0 else constants.GREEN
             
             segment = Actor()
             segment.set_position(position)
             segment.set_velocity(velocity)
             segment.set_text(text)
-            segment.set_color(color)
+            segment.set_color(self._cycle_color)# use author´s color
             self._segments.append(segment)
