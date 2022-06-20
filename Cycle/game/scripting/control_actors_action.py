f8a069b9c8c83e584a1a4c6a96783f286b2f22d8
Gabe G <97242028+josegabrielguerrero26@users.noreply.github.com>
2022-06-20 16:41:49 -0500

    controls

--- Cycle/game/scripting/control_actors_action.py
+++ Cycle/game/scripting/control_actors_action.py
@@ -5,9 +5,9 @@ from game.shared.point import Point
 
 class ControlActorsAction(Action):
     """
-    An input action that controls the snake.
+    An input action that controls the cycle.
     
-    The responsibility of ControlActorsAction is to get the direction and move the snake's head.
+    The responsibility of ControlActorsAction is to get the direction and move the cycle's head.
 
     Attributes:
         _keyboard_service (KeyboardService): An instance of KeyboardService.
@@ -20,8 +20,8 @@ class ControlActorsAction(Action):
             keyboard_service (KeyboardService): An instance of KeyboardService.
         """
         self._keyboard_service = keyboard_service
-        self._direction1 = Point(constants.CELL_SIZE, 0) #directions  cycle 1
-        self._direction2 = Point(constants.CELL_SIZE, 0) #directions  cycle 2
+        self._direction1 = Point(constants.CELL_SIZE, 0) #directions for cycle 1
+        self._direction2 = Point(constants.CELL_SIZE, 0) #directions for cycle 2
 
     def execute(self, cast, script):
         """Executes the control actors action.
@@ -30,29 +30,29 @@ class ControlActorsAction(Action):
             cast (Cast): The cast of Actors in the game.
             script (Script): The script of Actions in the game.
         """
-        cycles= cast.get_actors("cycle") # number of cycle
-        
-        """Cycle 1 """
+        cycles = cast.get_actors("cycles") #distinguish which cycle from list
+
+        # Cycle 1 keyboard directions
         # left
         if self._keyboard_service.is_key_down('a'):
-            self._direction = Point(-constants.CELL_SIZE, 0)
+            self._direction1 = Point(-constants.CELL_SIZE, 0)
         
         # right
         if self._keyboard_service.is_key_down('d'):
-            self._direction = Point(constants.CELL_SIZE, 0)
+            self._direction1 = Point(constants.CELL_SIZE, 0)
         
         # up
         if self._keyboard_service.is_key_down('w'):
-            self._direction = Point(0, -constants.CELL_SIZE)
+            self._direction1 = Point(0, -constants.CELL_SIZE)
         
         # down
         if self._keyboard_service.is_key_down('s'):
-            self._direction = Point(0, constants.CELL_SIZE)
+            self._direction1 = Point(0, constants.CELL_SIZE)
         
         cycle1 = cycles[0]
         cycle1.turn_head(self._direction1)
-        
-        """Cycle 2 """
+
+        # Added Cycle 2 keyboard directions
         # left
         if self._keyboard_service.is_key_down('j'):
             self._direction2 = Point(-constants.CELL_SIZE, 0)
@@ -70,4 +70,4 @@ class ControlActorsAction(Action):
             self._direction2 = Point(0, constants.CELL_SIZE)
         
         cycle2 = cycles[1]
-        cycle2.turn_head(self._direction2) 
+        cycle2.turn_head(self._direction2)
--- Cycle/game/scripting/draw_actors_action.py
+++ Cycle/game/scripting/draw_actors_action.py
@@ -18,6 +18,7 @@ class DrawActorsAction(Action):
             video_service (VideoService): An instance of VideoService.
         """
         self._video_service = video_service
+        self._times = 0 #added timer
 
     def execute(self, cast, script):
         """Executes the draw actors action.
@@ -26,6 +27,7 @@ class DrawActorsAction(Action):
             cast (Cast): The cast of Actors in the game.
             script (Script): The script of Actions in the game.
         """
+
         score = cast.get_first_actor("scores")
         cycles = cast.get_actors("cycles")
         cycle1 = cycles[0]
