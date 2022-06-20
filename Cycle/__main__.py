92c3ae70fa1cf482a80a19d765b3466c5026eec1
Gabe G <97242028+josegabrielguerrero26@users.noreply.github.com>
2022-06-20 16:23:29 -0500

    changes main

--- Cycle/__main__.py
+++ Cycle/__main__.py
@@ -1,7 +1,6 @@
 import constants
 
 from game.casting.cast import Cast
-from game.casting.food import Food
 from game.casting.score import Score
 from game.casting.cycle import Cycle
 from game.scripting.script import Script
@@ -9,21 +8,20 @@ from game.scripting.control_actors_action import ControlActorsAction
 from game.scripting.move_actors_action import MoveActorsAction
 from game.scripting.handle_collisions_action import HandleCollisionsAction
 from game.scripting.draw_actors_action import DrawActorsAction
+from game.scripting.grow_tail_action import GrowTailAction
 from game.directing.director import Director
 from game.services.keyboard_service import KeyboardService
 from game.services.video_service import VideoService
-from game.shared.color import Color
-from game.shared.point import Point
-
+from game.services.keyboard_service import KeyboardService
+from game.services.video_service import VideoService
 
 def main():
     
     # create the cast
     cast = Cast()
-    cast.add_actor("foods", Food())
-    cast.add_actor("snakes", Cycle())
+    cast.add_actor("cycles", Cycle(constants.RED)) # Added cycles
+    cast.add_actor("cycles", Cycle(constants.GREEN)) # Added cycles
     cast.add_actor("scores", Score())
-   
     # start the game
     keyboard_service = KeyboardService()
     video_service = VideoService()
@@ -31,6 +29,7 @@ def main():
     script = Script()
     script.add_action("input", ControlActorsAction(keyboard_service))
     script.add_action("update", MoveActorsAction())
+    script.add_action("update", GrowTailAction()) # tail adding class
     script.add_action("update", HandleCollisionsAction())
     script.add_action("output", DrawActorsAction(video_service))
     
