090b1d535c62280b4607e2e7de11b93edc43869b
Gabe G <97242028+josegabrielguerrero26@users.noreply.github.com>
2022-07-08 10:10:16 -0500

    casting changes

--- batter/game/casting/stats.py
+++ batter/game/casting/stats.py
@@ -8,58 +8,40 @@ class Stats(Actor):
     def __init__(self, debug = False):
         """Constructs a new Stats."""
         super().__init__(debug)
-        self._level = 1
-        self._lives = DEFAULT_LIVES
-        self._score = 0
+        self._score1 = 0
+        self._score2 = 0
+        # self._level = 1
+        # self._lives = DEFAULT_LIVES
+        # self._score = 0
 
-    def add_life(self):
-        """Adds one life."""
-        if self._lives < MAXIMUM_LIVES:
-            self._lives += 1 
-
-    def add_points(self, points):
-        """Adds the given points to the score.
+    def add_points(self, points, player):
+        """Adds the given points to the score of a player.
         
         Args:
             points: A number representing the points to add.
+            player: The player's number, either 1 or 2.
         """
-        self._score += points
-
-    def get_level(self):
-        """Gets the level.
-
-        Returns:
-            A number representing the level.
-        """
-        return self._level
-
-    def get_lives(self):
-        """Gets the lives.
-
-        Returns:
-            A number representing the lives.
-        """
-        return self._lives
+        if player == 1:
+            self._score1 += points
+        elif player == 2:
+            self._score2 += points
   
-    def get_score(self):
-        """Gets the score.
+    def get_score(self, player):
+        """Gets the players score.
+
+        Args:
+            player: The player's number, either 1 or 2.
 
         Returns:
             A number representing the score.
         """
-        return self._score
+        if player == 1:
+            return self._score1
+        elif player == 2:
+            return self._score2
 
-    def lose_life(self):
-        """Removes one life."""
-        if self._lives > 0:
-            self._lives -= 1
-    
-    def next_level(self):
-        """Adds one level."""
-        self._level += 1
 
     def reset(self):
         """Resets the stats back to their default values."""
-        self._level = 1
-        self._lives = DEFAULT_LIVES
-        self._score = 0
+        self._score1 = 0
+        self._score2 = 0
